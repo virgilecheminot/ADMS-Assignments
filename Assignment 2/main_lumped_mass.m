8d07f16d04c3758430737944eafad7e5353add30
Virgile Cheminot <virgile.cheminot@gmail.com>
2024-12-26 12:47:24 +0100

    End of FEM lab 3: concentrated mass on a node

--- Assignment 2/main_lumped_mass.m
+++ Assignment 2/main_lumped_mass.m
@@ -16,7 +16,7 @@ Lmax = sqrt(pi^2/a/Omax * sqrt(EJ/m));
 disp(['Maximum element legnth recommended: ', num2str(Lmax)]);
 
 % load the input file and assemble the structure
-[file_i,xy,nnod,sizew,idb,ndof,incid,l,gamma,m,EA,EJ,posit,nbeam,pr]=loadstructure('ADMS_FEM_03');
+[file_i,xy,nnod,sizew,idb,ndof,incid,l,gamma,m,EA,EJ,posit,nbeam,pr]=loadstructure('ADMS_FEM_03_mass');
 
 % draw the structure
 dis_stru(posit,l,gamma,xy,pr,idb,ndof);
@@ -33,11 +33,13 @@ Ml_local = [ml  0   0
 Eml = zeros(3,size(M,1)); % expansion matrix
 indices = idb(5,:);
 Eml(:,indices) = eye(3);
+Eml_8 = zeros(3,size(M,1)); % expansion matrix for the 8th node
+Eml_8(:,idb(8,:)) = eye(3);
 
-Ml = Eml'*Ml_local*Eml;
+Ml = Eml'*Ml_local*Eml + Eml_8'*Ml_local*Eml_8;
 M_tot = M + Ml;
 
-% concentrated springs
+% concentrated springs (only at the 5th node)
 Kl_local = [kx 0
             0 ky];
 
@@ -46,6 +48,21 @@ Ek(:,idb(5,1:2)) = eye(2);
 Kl = Ek'*Kl_local*Ek;
 
 K_tot = K + Kl;
+
+% concentrated springs between nodes 2 and 8
+Kk_local = [1 0 0 -1 0 0]' * k * [1 0 0 -1 0 0];
+g = 3*pi/2;
+lambda_k = [cos(g)  sin(g) 0
+            -sin(g) cos(g) 0
+            0       0      1];
+Lambda_k = [lambda_k zeros(3)
+            zeros(3) lambda_k];
+Kk_G = Lambda_k' * Kk_local * Lambda_k;
+idofn2 = idb(2,:);
+idofn8 = idb(8,:);
+idofk = [idofn2 idofn8];
+
+K_tot(idofk,idofk) = K_tot(idofk,idofk) + Kk_G;
 
 %% Extract the free-free partition
 [MFF, MFC, MCC] = freefree(M_tot,ndof);
@@ -86,12 +103,12 @@ C = ab(1)*M_tot + ab(2)*K_tot;
 
 %% Frequency response function
 F0 = zeros(ndof,1);
-index = idb(4,2); % force applied at node 4 in the y direction
+index = idb(4,2);
 F0(index) = 1;
 
-om = (0:0.2:100)*2*pi; % radiants per second
+om = (0:0.2:100)*2*pi;
 
-X = zeros(ndof, length(om)); % Preallocate X
+X = zeros(ndof, length(om));
 for ii=1:length(om)
     A = -om(ii)^2*MFF + 1i*om(ii)*CFF + KFF;
     X(:,ii) = A\F0;
@@ -99,59 +116,15 @@ end
 
 % Plot the FRF
 figure
-subplot(2,1,1)  
-semilogy(om/(2*pi), abs(X(idb(5,2),:)), 'LineWidth', 1.5)
-xlabel('Frequency [Hz]')
-ylabel('Amplitude')
-title('Frequency Response Function')
-grid on
-hold on
-subplot(2,1,2)
-plot(om/(2*pi), angle(X(idb(5,2),:)), 'LineWidth', 1.5)  
-xlabel('Frequency [Hz]')
-ylabel('Phase [rad]')
-grid on
-hold on
-
-%% Concentrated spring between nodes 2 and 6
-K_k_local = [1 0 0 -1 0 0]' * k * [1 0 0 -1 0 0];
-g = 7*pi/4;
-lambda_k = [cos(g)  sin(g) 0
-            -sin(g) cos(g) 0
-            0       0      1];
-Lambda_k = [lambda_k zeros(3)
-            zeros(3) lambda_k];
-
-K_k_G = Lambda_k' * K_k_local * Lambda_k;
-idofn2 = idb(2,:);
-idofn6 = idb(6,:);
-idofk = [idofn2 idofn6];
-
-K_tot_k = K_tot;
-K_tot_k(idofk, idofk) = K_tot_k(idofk, idofk) + K_k_G;
-
-% Extract the free-free partition
-MFF_k = MFF;
-KFF_k = freefree(K_tot_k, ndof);
-
-% Compute the modes
-[x0_k, omega_squared_k] = eig(MFF_k\KFF_k);
-omega_k = diag(sqrt(omega_squared_k));
-[omega_k, ind_k] = sort(omega_k);
-modes_k = x0_k(:,ind_k);
-
-% Plot the first 3 modes
-figure
-for i = 1:3
-    mode = modes_k(:,i);
-    diseg2(mode, scale_factor, incid, l, gamma, posit, idb, xy);
-end
-
-% plot the mode frequencies
-figure
-stem(omega_k/(2*pi), 'filled', 'LineWidth', 1.5);
-yscale('log');
-xlabel('Mode number');
-ylabel('Frequency [Hz]');
-title('Mode frequencies');
+subplot(2,1,1);
+semilogy(om/(2*pi), abs(X(idb(5,2),:)), 'LineWidth', 1.5);
+xlabel('Frequency [Hz]');
+ylabel('Amplitude');
+title('Frequency response function');
 grid on;
+subplot(2,1,2);
+plot(om/(2*pi), angle(X(idb(5,2),:)), 'LineWidth', 1.5);
+xlabel('Frequency [Hz]');
+ylabel('Phase [rad]');
+grid on;
+
